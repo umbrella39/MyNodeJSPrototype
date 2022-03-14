85db409270dfe609be9474182302c45c774b2457
umbrella39 <umbrella39@users.noreply.github.com>
2022-03-14 17:59:11 +0900

    DB接続情報の外部ファイル化とクエリ実行部分の連携表示テスト

--- main.js
+++ main.js
@@ -2,16 +2,20 @@
 
 // Hello, Node.js World Sample:https://nodejs.org/ja/about/
 const http = require('http');
-// DB Operation(by using Test or Prototyping)
-const db = require('./plugins/testDbFunc.js');
-
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// DB Operation(by using Test or Prototyping)
+const db = require('./plugins/testDbFunc.js');
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+  console.log(db.thisResult);
+//  res.write(db.res.rows[0].user_name);
+//  res.write('<meta charset="UTF-8">');
+//  res.end('Hello World');
+  res.end(db.thisResult);
 });
 
 server.listen(port, hostname, () => {
---
+++ plugins/testDbFunc.js
@@ -0,0 +1,19 @@
+'use strict';
+// include pg
+const { Pool, Client } = require('pg')
+// DB Connect Info(by using Test or Prototyping)
+const dbConnect = require('./dbConfig.js');
+
+dbConnect.pool.query(dbConnect.SQL_STATEMENT, (err, res) => {
+    //console.log(err, res)
+    dbConnect.pool.end()
+})
+
+dbConnect.client.connect()
+
+dbConnect.client.query(dbConnect.SQL_STATEMENT, (err, res) => {
+//  console.log(err, res)
+  exports.thisResult = res.rows[0].user_name
+  //console.log(thisResult)
+  dbConnect.client.end()
+})
